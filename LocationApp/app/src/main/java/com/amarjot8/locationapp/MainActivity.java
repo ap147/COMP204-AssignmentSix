54c0014049d68d90b9faf6a1a630fa3b0c5a0459
Parmar, Amarjot <amarjot-parmar@hotmail.co.nz>
2016-09-17 04:15:16 +1200

    Log message appears, helped method added also

--- LocationApp/app/src/main/java/com/amarjot8/locationapp/MainActivity.java
+++ LocationApp/app/src/main/java/com/amarjot8/locationapp/MainActivity.java
@@ -2,12 +2,52 @@ package com.amarjot8.locationapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    //Add code to your project that outputs messages to the debug log when a lifecycle method is called
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //Use the tag Lifecycle for these messages
+        //Simply printing the name of the called method will be sufficient
+        updateLog("Lifecycle", "onStart");
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateLog("Lifecycle", "onResume");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        updateLog("Lifecycle","onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        updateLog("Lifecycle","onStop");
+        super.onStop();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        updateLog("Lifecycle","onCreate");
+    }
+
+    protected void updateLog(String tag, String message)
+    {
+        //printing what lifecycle method was called
+        Log.e(tag, message);
+        try {
+            //Slowing it down so they all come in order
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
