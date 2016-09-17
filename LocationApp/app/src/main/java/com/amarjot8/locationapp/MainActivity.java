fd190d17d2dd5cf8fcab3e684b92ad9ee78876dc
Parmar, Amarjot <amarjot-parmar@hotmail.co.nz>
2016-09-17 15:34:36 +1200

    Lon / Lat is rounded down to 4 dp

--- LocationApp/app/src/main/java/com/amarjot8/locationapp/MainActivity.java
+++ LocationApp/app/src/main/java/com/amarjot8/locationapp/MainActivity.java
@@ -1,18 +1,24 @@
 package com.amarjot8.locationapp;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.icu.text.DecimalFormat;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+
+import java.math.RoundingMode;
+
 //https://www.youtube.com/watch?v=QNb_3QKSmMk
 public class MainActivity extends AppCompatActivity {
     LocationManager locationManager;
@@ -204,17 +210,20 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
     }
+    @TargetApi(Build.VERSION_CODES.N)
     protected void updateLat_val(double lat)
     {
+        double roundedDown = (double)Math.round(lat * 10000d) / 10000d;
         //Getting textview & changing status
         TextView textView = (TextView) findViewById(R.id.lat_val);
-        String x = "" + lat;
+        String x = "" + roundedDown;
         textView.setText(x);
     }
     protected void updateLon_val(double lon){
+        double roundedDown = (double)Math.round(lon * 10000d) / 10000d;
         //Getting textview & changing status
         TextView textView = (TextView) findViewById(R.id.lon_val);
-        String x = "" + lon;
+        String x = "" + roundedDown;
         textView.setText(x);
     }
 }
